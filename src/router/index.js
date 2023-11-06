12fd305ac4abaa5ad60735e72d8a9eb096500932
Tzubast <121686868+Tzubast@users.noreply.github.com>
2023-11-06 23:31:05 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/calc.vue'
+import HomeView from '../views/Calc.vue'
 import 'font-awesome/css/font-awesome.css';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
